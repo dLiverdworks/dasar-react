f94c22804727c0327f1392d4b5ae0837fb5e9972
DESKTOP-BDE2T1R\mhuss <mhussuduri@gmail.com>
2025-06-12 21:35:08 +0700

    Create contact component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Contact name="Bryan Curry" phone="082229928292"/>
+      <Contact name="Gibran" phone="082229928291"/>
+      <Contact name="Bahlil" phone="082229928293"/>
     </div>
   );
 }
