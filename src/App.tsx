db862d2059653e233c08cf22eb25e4dad0c25b73
DESKTOP-BDE2T1R\mhuss <mhussuduri@gmail.com>
2025-06-12 22:15:08 +0700

    finishing render list

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,29 @@ import React from "react";
 import Contact from "./components/Contact";
 
 function App() {
+  const contactItems = [
+    {
+      id: 1,
+      name: "Bryan Curry",
+      phone: "082229928292",
+    },
+    {
+      id: 2,
+      name: "Gibran",
+      phone: "082229928291",
+    },
+    {
+      id: 3,
+      name: "Bahlil",
+      phone: "082229928293",
+    },
+  ];
+
   return (
     <div>
-      <Contact name="Bryan Curry" phone="082229928292"/>
-      <Contact name="Gibran" phone="082229928291"/>
-      <Contact name="Bahlil" phone="082229928293"/>
+      {contactItems.map((item) => (
+        <Contact key={item.id} name={item.name} phone={item.phone} />
+      ))}
     </div>
   );
 }
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 type Props = {
-    name: string;
-    phone: string;
-}
+  name: string;
+  phone: string;
+};
 
 const Contact: React.FC<Props> = ({ name, phone }) => {
+  const [isPhoneCensored, setIsPhoneCensored] = useState(false);
+
   return (
     <div
       style={{
         padding: "16px 0px",
         borderBottom: "1px solid rgba(221, 221, 221, 1)",
-        marginBottom: "16px",
       }}
+      onClick={() => setIsPhoneCensored(!isPhoneCensored)}
     >
       <p
         style={{
@@ -24,14 +26,26 @@ const Contact: React.FC<Props> = ({ name, phone }) => {
       >
         {name}
       </p>
-      <p
-        style={{
-          fontSize: "16px",
-          color: "rgba(135, 152, 186, 1)",
-        }}
-      >
-        {phone}
-      </p>
+      {!isPhoneCensored && (
+        <p
+          style={{
+            fontSize: "16px",
+            color: "rgba(135, 152, 186, 1)",
+          }}
+        >
+          {phone}
+        </p>
+      )}
+      {isPhoneCensored && (
+        <p
+          style={{
+            fontSize: "16px",
+            color: "rgba(135, 152, 186, 1)",
+          }}
+        >
+          ************
+        </p>
+      )}
     </div>
   );
 };
